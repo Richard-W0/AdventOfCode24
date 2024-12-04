73741573cd5f7b88b9e651b5e1c32a75de24dcb9
Richard-W0 <rikuwong@hotmail.fi>
2024-12-04 14:06:04 +0200

    almost works now lol

--- Day3/day3.f90
+++ Day3/day3.f90
@@ -1,50 +1,47 @@
 program day3
   implicit none
   integer :: ios, total, x, y, alotus, lopetus, tiedostoPituus, i, kerto, pilkku
+  logical :: enabled
   character(len = 10) :: tiedosto
   character(len = :), allocatable :: temp, ehdokas
   character(len=1), allocatable :: buffer(:)
 
   total = 0
   alotus = 1
+  enabled = .true.
 
   tiedosto = "paiva3.txt"
 
   open(unit=10, file=tiedosto, status="old", access="stream", action="read", iostat=ios)
-  if (ios /= 0) then
-    print *, "Error reading file"
-    stop  
-  end if
+  if (ios /= 0) stop
 
-  inquire(unit=10, size=tiedostoPituus) !not entirely sure how this works but it works
-
+  inquire(unit=10, size=tiedostoPituus) !not exactly sure how this works, but it does
   allocate(buffer(tiedostoPituus))
 
   read(10) buffer
   close(10)
 
-  temp=""
-
+  temp = ""
   do i = 1, size(buffer)
     temp = trim(temp) // buffer(i)
   end do
 
- do
+  do
     kerto = index(temp(alotus:), "mul(")
-    if (kerto == 0) exit 
+    if (kerto == 0) exit
     kerto = alotus + kerto - 1
+
+    if (index(temp(alotus:), "do()") > 0 .and. index(temp(alotus:), "do()") < kerto) enabled = .true.
+    if (index(temp(alotus:), "don't()") > 0 .and. index(temp(alotus:), "don't()") < kerto) enabled = .false.
 
     lopetus = index(temp(kerto:), ")")
     if (lopetus > 12) then
-      print *, lopetus
-      alotus =  kerto + 4
+      alotus = kerto + 4
       cycle
     end if
-
-    lopetus = kerto + lopetus -1
+    lopetus = kerto + lopetus - 1
 
     ehdokas = temp(kerto:lopetus)
-    
     if (ehdokas(1:4) /= "mul(") then
       alotus = kerto + 4
       cycle
@@ -58,22 +55,21 @@ program day3
 
     read(ehdokas(5:pilkku-1), *, iostat=ios) x
     if (ios /= 0) then
-      print *, "Invalid x in", ehdokas
       alotus = lopetus + 1
       cycle
     end if
       
     read(ehdokas(pilkku+1:len(ehdokas)-1), *, iostat=ios) y
     if (ios /= 0) then
-      print *, "Invalid y in", ehdokas
       alotus = lopetus + 1
       cycle
     end if
-    print *, y
-    total = total + (x * y)
-    alotus = lopetus + 1
 
-      end do
+    if (enabled) total = total + (x * y)
+    alotus = lopetus + 1
+  end do
 
   print *, total
 end program day3
+
+
